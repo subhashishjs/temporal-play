03677b1b24a6dcbcc2843c92e8b8d506d5871956
Subhashish Jung Shah <subhashishjungshah@Subhashishs-MacBook-Pro.local>
2025-12-14 17:44:47 +0545

    added braintrust for logging

--- run_worker.py
+++ run_worker.py
@@ -3,9 +3,22 @@ import concurrent.futures
 from temporalio.client import Client
 from temporalio.worker import Worker
 
+# Braintrust logger for stimulation
+import braintrust
+from braintrust.wrappers.openai import BraintrustTracingProcessor
+
+from agents import set_trace_processors
+
 # Import the activity and workflow from our other files
 from activities import execute_agent
 from workflows import AgentExecution
+
+
+temporal_playground_logger = braintrust.init_logger(
+    project="temporal-playground",
+)
+
+set_trace_processors([BraintrustTracingProcessor(logger=temporal_playground_logger)])
 
 async def main():
     # Create client connected to server at the given address
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 <!-- Temporal Workflow Ideas -->
 
 - Create a sample application to mimic our current agent-execution
-  - First I want to create a workflow that mimics our current orchestor.py by breaking into steps
+  - First I want to create a workflow that mimics our current orchestor.py by breaking into steps [completed]
   - Then I want to create the agent execution as simple as we have for our current agents
+  - Add braintrust logger for stimulation
 - Create a simple group chat workflow to understand how can we add a sliding window or the message states
